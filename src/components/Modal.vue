063a120602aed20e09b0c506c11d55cd7a720f96
Yuriy Martini <y.martini@soluzionesoftware.com>
2020-03-11 12:42:17 +0100

    Added close button and listener to Modal component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,12 @@
 <template>
     <component :is="usePortal ? 'portal' : 'div'" :to="portalTarget">
         <div v-if="visible" class="fixed inset-0 flex items-center justify-center z-50">
+            <div class="absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white z-50 bg-black opacity-75" @click="hide" title="(Esc)">
+                <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                    <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                </svg>
+            </div>
+
             <transition
                     @before-leave="backdropLeaving = true"
                     @after-leave="backdropLeaving = false"
@@ -64,6 +70,12 @@
                 cardLeaving: false,
             }
         },
+        mounted() {
+            document.addEventListener("keydown", this.keydownListener);
+        },
+        destroyed() {
+            document.removeEventListener("keydown", this.keydownListener);
+        },
         methods: {
             show(){
                 this.visible = true;
@@ -71,6 +83,12 @@
             hide(){
                 this.visible = false;
             },
+            keydownListener(e){
+                // Close modal with 'esc' key
+                if (("key" in e && (e.key === "Escape" || e.key === "Esc")) || e.keyCode === 27) {
+                    this.hide();
+                }
+            }
         }
     }
 </script>
